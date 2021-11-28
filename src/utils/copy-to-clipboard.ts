4d92651b59e24344edb938df714a998918e7d12c
Bluecrow UBUNTU DESKTOP <bluecrow.dev@gmail.com>
2021-11-28 18:16:00 +0000

    Added copy to clipboard util

---
+++ src/utils/copy-to-clipboard.ts
@@ -0,0 +1,9 @@
+export const copyToClipboard = (toCopy: any): void => {
+  if (!toCopy) {
+    alert("Anything to copy");
+    return;
+  }
+  navigator.clipboard.writeText(toCopy);
+  alert("Copied to clipboard");
+  return;
+};
